9a9a1f7407055ca3d3585c4a016ba8872fd8e325
tobias <tobias.omondi@student.moringaschool.com>
2023-07-21 21:40:19 +0300

    my-assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  // your code here 
+  document.querySelector('form').addEventListener('submit', (event) => {
+    event.preventDefault()
+    console.log(event)
+    let input = document.getElementById('new-task-description').value
+    let li = document.createElement('li')
+    li.textContent=input
+    document.querySelector('#tasks').appendChild(li)
+    document.querySelector('form').reset()
+
+//handle delete form
+let deleteButton= document.createElement('button')
+  li.appendChild(deleteButton)
+  deleteButton.textContent = "x"
+  document.querySelector('form').reset()
+  function addEventListenerToButton(){
+    deleteButton.addEventListener('click', function(e){
+      e.target.parentNode.remove()
+    })
+    
+  }
+  addEventListenerToButton() 
+
+})
+  
 });
